744a5054d1c9a0a9faa5c19c4fcdca16d1da706e
banil2026 <anilbogathi2004@gmail.com>
2026-07-21 09:52:02 +0530

    login functionality  added

---
+++ sample.py
@@ -0,0 +1,8 @@
+def login(user_name,user_password):
+    if user_name=="banil2026":
+        if user_password=="banil100":
+            return "login successfull"
+        else:
+            return "incorrect password"
+    else:
+        return "incorrect username"
